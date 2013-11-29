3680ba96d329c8d08c6e95a76d4fc3354d82cbec
Andrew Smith <smith.andrew.l@gmail.com>
2013-11-28 20:54:55 -0600

    Added generator.h

--- Makefile
+++ Makefile
@@ -5,7 +5,10 @@ MAKEFLAGS = -j5
 all: henley.cpp tests
 	$(CC) $(CFLAGS) henley.cpp -o henley
 
-tests: tests.cpp
+tests: tests.cpp generator.h
 	$(CC) $(CFLAGS) -lboost_system -lboost_unit_test_framework tests.cpp -o tests
+
+generator.h: generator.h
+	$(CC) $(CFLAGS) -c generator.h
 clean:
 	rm -f henley tests
---
+++ generator.h
@@ -0,0 +1,27 @@
+#include <map>
+#include <utility>
+#include <vector>
+#include <queue>
+
+using std::map;
+using std::vector;
+using std::pair;
+using std::priority_queue;
+
+template<typename S>
+class Generator {
+public:
+	Generator() { }
+	void init(vector<S> items) { }
+
+	vector<S> generate(int count) { return vector<S>(); }
+
+protected:
+	map<pair<S, S>, priority_queue<pair<S, int>>> build(vector<S>) {
+		return map<pair<S, S>, priority_queue<pair<S, int>>>();
+	}
+
+private:
+	map<pair<S, S>, priority_queue<pair<S, int>>> table;
+
+};
