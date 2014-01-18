e30ead02bf0af0b4a6d99954f1e03db48b7339ff
Andrew Smith <smith.andrew.l@gmail.com>
2014-01-18 17:45:04 -0600

    Removed outdated tests of generator

--- tests.cpp
+++ tests.cpp
@@ -9,46 +9,6 @@
 using std::string;
 using std::vector;
 
-class IntGeneratorTest : public ::testing::Test {
-protected:
-	virtual void SetUp() {
-		empty.init(vector<int>());
-		small.init({1, 4, 1, 1, 4, 5, 1, 4, 1});
-	}
-
-	Generator<int> empty;
-	Generator<int> small;
-};
-
-TEST_F(IntGeneratorTest, EmptyInit) {
-	EXPECT_EQ(vector<int>(), empty.generate(10, std::make_pair(0, 0)));
-}
-
-TEST_F(IntGeneratorTest, GenerateZero) {
-	EXPECT_EQ(vector<int>(), empty.generate(0, std::make_pair(0, 0)));
-	EXPECT_EQ(vector<int>(), small.generate(0, std::make_pair(1, 4)));
-}
-
-TEST_F(IntGeneratorTest, GenerateThree) {
-	vector<int> expected = {1, 1, 4};
-
-	EXPECT_EQ(expected, small.generate(3, std::make_pair(1, 4)));
-}
-
-TEST_F(IntGeneratorTest, GenerateSix) {
-	vector<int> expected = {1, 4, 1, 1, 4, 1};
-
-	EXPECT_EQ(expected, small.generate(6, std::make_pair(4,5)));
-}
-
-TEST(TextGenerator, EmptyInit) {
-	TextGenerator textGenerator;
-
-	textGenerator.init(vector<string>());
-
-	EXPECT_EQ(vector<string>(), textGenerator.generate(6, std::make_pair("one", "two")));
-}
-
 TEST(Split, EmptyString) {
 	EXPECT_EQ(vector<string>(), split("", " "));
 }
